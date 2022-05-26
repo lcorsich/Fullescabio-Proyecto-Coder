45580148a98846cf3bf57a15542e5b7a3778e1d6
lcorsich <lautarocorsich@yahoo.com.ar>
2022-05-26 14:01:20 -0300

    Segunda Clase

---
+++ src/components/NavBar/NavBar.js
@@ -0,0 +1,30 @@
+const NavBar = () => {
+  return (
+    <div class="title">
+      <nav>
+        <h1
+          style={{
+            backgroundColor: "black",
+            color: "white",
+            padding: "10px",
+            fontFamily: "Sans-Serif",
+          }}
+        >
+          FULLESCABIO
+        </h1>
+        <h3>Almacen de bebibas</h3>
+        <div class="buttonstConteiner" style={{backgroundColor: "black"}}>
+          <button class='btnCategoria' >Vinos</button>
+          <button class='btnCategoria'>Espumantes</button>
+          <button class='btnCategoria'>Whiskies</button>
+          <button class='btnCategoria'>Gin</button>
+          <button class='btnCategoria'>Aperitivos</button>
+          <button class='btnCategoria'>Ron</button>
+          <button class='btnCategoria'>Vodka</button>
+          <button class='btnCategoria'>Cervezas</button>
+        </div>
+      </nav>
+    </div>
+  );
+};
+export default NavBar;
